dadcfae77b7de8f3cf7b9adbe8e1a625150cae3e
VitalyShpak9222 <112765255+VitalyShpak9222@users.noreply.github.com>
2022-09-17 22:29:16 +0300

    Task 1 has been completed

--- KW_at_17_09_2022/Program.cs
+++ KW_at_17_09_2022/Program.cs
@@ -8,7 +8,7 @@ namespace KW_at_17_09_2022
 {
     class Program
     {
-        struct Person
+        struct Student
         {
             public string firstname;
             public string surname;
@@ -23,6 +23,40 @@ namespace KW_at_17_09_2022
         }
         static void Main(string[] args)
         {
+            Student Maxim = new Student();
+            Maxim.firstname = "Максим";
+            Maxim.surname = "Романов";
+            Maxim.identifier = 1;
+            Maxim.category_alcohol = 'b';
+            Maxim.volume = 872;
+            Student Ilia = new Student();
+            Ilia.firstname = "Илья";
+            Ilia.surname = "Колобков";
+            Ilia.identifier = 2;
+            Ilia.category_alcohol = 'c';
+            Ilia.volume = 1238;
+            Student Renat = new Student();
+            Renat.firstname = "Ренат";
+            Renat.surname = "Ахмеджан";
+            Renat.identifier = 3;
+            Renat.category_alcohol = 'a';
+            Renat.volume = 820;
+            Student Karim = new Student();
+            Karim.firstname = "Карим";
+            Karim.surname = "Мусаголиев";
+            Karim.identifier = 4;
+            Karim.category_alcohol = 'd';
+            Karim.volume = 243;
+            Student Kamila = new Student();
+            Kamila.firstname = "Камила";
+            Kamila.surname = "Уткина";
+            Kamila.identifier = 5;
+            Kamila.category_alcohol = 'a';
+            Kamila.volume = 68;
+            double alkovolume = Maxim.volume + Ilia.volume + Renat.volume + Karim.volume + Kamila.volume;
+            Console.WriteLine("Общий объем выпитого алкоголя равен " + alkovolume + "\nМаксим выпил " + (Maxim.volume / alkovolume + "\nИлья выпил " + (Ilia.volume / alkovolume) + "\r\nРенат выпил " + (Renat.volume / alkovolume) + "\r\nКарим выпил " + (Karim.volume / alkovolume) + "\r\nКамила выпила " + (Kamila.volume / alkovolume)));
+            Console.ReadKey();
+
 
         }
     }
