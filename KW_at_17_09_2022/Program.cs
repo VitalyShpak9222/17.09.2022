690eb81cbed174aaac45ee7db022b3172b51f903
VitalyShpak9222 <112765255+VitalyShpak9222@users.noreply.github.com>
2022-09-17 21:22:42 +0300

    Add project files.

---
+++ KW_at_17_09_2022/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KW_at_17_09_2022
+{
+    class Program
+    {
+        struct Person
+        {
+            public string firstname;
+            public string surname;
+            public int identifier;
+            public char category_alcohol;
+            public int volume;
+
+            public void Print()
+            {
+                Console.WriteLine($"Имя: {firstname}  Фамилия: {surname} Индификатор: {identifier} Категория: {category_alcohol} Объем выпитого {volume}");
+            }
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
